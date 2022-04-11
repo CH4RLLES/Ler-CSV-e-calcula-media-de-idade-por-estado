dee523464165afbe42e3fd1aa0ab8730a92f5689
unknown <charles.sousa@sindifisconacional.org.br>
2022-04-11 11:35:11 -0300

    inicial

---
+++ estudos1/Program.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculeMediaDeIdadePorEstado();
+        Console.WriteLine();
+        ConverteCsvParaJson();
+
+        Console.ReadKey();
+
+        static void CalculeMediaDeIdadePorEstado()
+        {
+            var csv = File
+             .ReadAllLines(@"c:/input.csv")
+             .Select(x => x.Split(','))
+             .Select((x, i) => new
+             {
+                 Cidade = x[1],
+                 Idade = Decimal.Parse(x[2])
+             })
+             .GroupBy(x => x.Cidade)
+             .Select(g => new
+             {
+                 cidade = g.Key,
+                 idade = Math.Round(g.Average(c => c.Idade), 2)
+             });
+
+            foreach (var item in csv)
+            {
+                Console.WriteLine(item.cidade+", "+item.idade);
+            }
+        }
+
+        static void ConverteCsvParaJson()
+        {
+            var medias = File
+             .ReadAllLines(@"c:/input.csv")
+             .Select(x => x.Split(','))
+             .Select((x, i) => new
+             {
+                 Cidade = x[1],
+                 Idade = Decimal.Parse(x[2])
+             })
+             .GroupBy(x => x.Cidade)
+             .Select(g => new
+             {
+                 cidade = g.Key,
+                 idade = Math.Round(g.Average(c => c.Idade), 2)
+             });
+
+            var json = JsonConvert.SerializeObject(new { medias });
+            Console.WriteLine(json);
+            
+        }
+
+        static async Task EnviaJsonViaPost(string json)
+        {
+            var client = new HttpClient();
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("https://zeit-endpoint.brmaeji.now.sh/api/avg", content);
+
+            if (response.Content != null)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
